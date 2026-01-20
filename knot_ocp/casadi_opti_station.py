36ff5286b7c7f5f88a38aa1894f17c9d2cb06c86
bapodacaumich <114904896+bapodacaumich@users.noreply.github.com>
2026-01-20 14:44:07 -0500

    Add license and description to casadi_opti_station.py
    Added licensing information and program description.

--- knot_ocp/casadi_opti_station.py
+++ knot_ocp/casadi_opti_station.py
@@ -1,3 +1,22 @@
+"""
+space station inspection planning using optimal control problem
+Copyright (C) 2026 Brandon Apodaca
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 from casadi import *
 from ode import ode_funCW
 from utils import plot_solution3_convex_hull, filter_path_na, compute_time_intervals, compute_time_intervals_fixed, linear_initial_path, num2str
